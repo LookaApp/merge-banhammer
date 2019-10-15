6f8701c3bf28d26a80e6624698b912676ce4b71f
Ryan De Villa <ryandv@logojoy.com>
2019-10-15 11:46:50 -0400

    introduce logging and error handling into banService

--- mergeban/ban_service.go
+++ mergeban/ban_service.go
@@ -1,14 +1,21 @@
 package mergeban
 
 import (
+	"log"
 	"net/http"
+	"os"
 )
 
 type banService struct {
+	logger     *log.Logger
+	mergeQueue []string
 }
 
 func CreateBanService() *banService {
-	return &banService{}
+	return &banService{
+		logger:     log.New(os.Stdout, "", log.Ldate|log.Ltime),
+		mergeQueue: make([]string, 0, 12),
+	}
 }
 
 type BanHammer interface {
@@ -17,5 +24,9 @@ type BanHammer interface {
 
 func (b *banService) Ban(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
-	w.Write([]byte("Hello World!"))
+	_, err := w.Write([]byte("You have the merge banhammer!"))
+
+	if err != nil {
+		b.logger.Printf("Failed to write response body: %v\n", err)
+	}
 }
