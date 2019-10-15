16e88be7d31702cf042827c956a3f3759884825f
Ryan De Villa <ryandv@logojoy.com>
2019-10-15 11:52:02 -0400

    implement queueing of merges from different users

--- mergeban/ban_service.go
+++ mergeban/ban_service.go
@@ -1,6 +1,7 @@
 package mergeban
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -24,9 +25,21 @@ type BanHammer interface {
 
 func (b *banService) Ban(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
-	_, err := w.Write([]byte("You have the merge banhammer!"))
+
+	_, err := w.Write([]byte(b.EnqueueMerge()))
 
 	if err != nil {
 		b.logger.Printf("Failed to write response body: %v\n", err)
 	}
+}
+
+func (b *banService) EnqueueMerge() string {
+	b.mergeQueue = append(b.mergeQueue, "foobar")
+	queueLength := len(b.mergeQueue)
+
+	if queueLength == 1 {
+		return "You have the merge banhammer!"
+	}
+
+	return fmt.Sprintf("The banhammer has already been taken. Your position: [%v/%v]\n", queueLength, queueLength)
 }
--- test/ban_service_test.go
+++ test/ban_service_test.go
@@ -12,9 +12,8 @@ import (
 )
 
 func TestBanEndpoint(t *testing.T) {
-	banService := mergeban.CreateBanService()
-
-	t.Run("/ban", func(t *testing.T) {
+	t.Run("/ban - successfully acquiring initial lock", func(t *testing.T) {
+		banService := mergeban.CreateBanService()
 		requestBody := strings.NewReader("user_id=42")
 		request := httptest.NewRequest("POST", "/ban", requestBody)
 		w := httptest.NewRecorder()
@@ -24,6 +23,25 @@ func TestBanEndpoint(t *testing.T) {
 		responseBody, _ := ioutil.ReadAll(response.Body)
 
 		assert.Equal(t, 200, response.StatusCode)
-		assert.NotEmpty(t, responseBody)
+		assert.Equal(t, "You have the merge banhammer!", string(responseBody))
+	})
+
+	t.Run("/ban - queueing to acquire lock if it has already been taken", func(t *testing.T) {
+		banService := mergeban.CreateBanService()
+		requestBody := strings.NewReader("user_id=23")
+		request := httptest.NewRequest("POST", "/ban", requestBody)
+		w := httptest.NewRecorder()
+		requestBody2 := strings.NewReader("user_id=42")
+		request2 := httptest.NewRequest("POST", "/ban", requestBody2)
+		w2 := httptest.NewRecorder()
+
+		banService.Ban(w, request)
+		banService.Ban(w2, request2)
+		response2 := w2.Result()
+
+		responseBody2, _ := ioutil.ReadAll(response2.Body)
+
+		assert.Equal(t, 200, response2.StatusCode)
+		assert.Equal(t, "The banhammer has already been taken. Your position: [2/2]\n", string(responseBody2))
 	})
 }
