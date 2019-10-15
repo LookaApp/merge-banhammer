2190e10d94d7f66b7f2dfcd14b9ec27259345292
Ryan De Villa <ryandv@logojoy.com>
2019-10-15 13:29:35 -0400

    introduce Lift method to BanHammer interface

--- mergeban/ban_service.go
+++ mergeban/ban_service.go
@@ -20,6 +20,7 @@ func CreateBanService(logger *log.Logger) *banService {
 
 type BanHammer interface {
 	Ban(w http.ResponseWriter, r *http.Request)
+	Lift(w http.ResponseWriter, r *http.Request)
 }
 
 func (b *banService) Ban(w http.ResponseWriter, r *http.Request) {
@@ -33,6 +34,20 @@ func (b *banService) Ban(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 	_, err = w.Write([]byte(b.enqueueMerge(userID)))
 
+	if err != nil {
+		b.logger.Printf("Failed to write response body: %v\n", err)
+	}
+}
+
+func (b *banService) Lift(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		b.logger.Printf("Failed to parse form data from POST request: %v\n", err)
+	}
+
+	w.WriteHeader(200)
+
+	_, err = w.Write([]byte("Hello world!"))
 	if err != nil {
 		b.logger.Printf("Failed to write response body: %v\n", err)
 	}
---
+++ test/lift_service_test.go
@@ -0,0 +1,40 @@
+package test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"mergeban/mergeban"
+)
+
+func TestLiftEndpoint(t *testing.T) {
+	t.Run("/lift - successfully releasing a held lock", func(t *testing.T) {
+		logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+		banService := mergeban.CreateBanService(logger)
+		w, request := createLiftRequest("42")
+
+		banService.Lift(w, request)
+		response := w.Result()
+		responseBody, _ := ioutil.ReadAll(response.Body)
+
+		assert.Equal(t, 200, response.StatusCode)
+		assert.NotEmpty(t, responseBody)
+	})
+}
+
+func createLiftRequest(userID string) (*httptest.ResponseRecorder, *http.Request) {
+	requestBody := strings.NewReader(fmt.Sprintf("user_id=%v", userID))
+	request := httptest.NewRequest("POST", "/lift", requestBody)
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	w := httptest.NewRecorder()
+
+	return w, request
+}
