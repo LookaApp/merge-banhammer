62dc1e93b9749fb0ca428a816e859ddf9bd8dd4e
Ryan De Villa <ryandv@logojoy.com>
2019-10-15 12:09:58 -0400

    prevent the same user from enqueuing twice

--- mergeban/ban_service.go
+++ mergeban/ban_service.go
@@ -24,22 +24,35 @@ type BanHammer interface {
 }
 
 func (b *banService) Ban(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(200)
+	err := r.ParseForm()
+	if err != nil {
+		b.logger.Printf("Failed to parse form data from POST request: %v\n", err)
+	}
 
-	_, err := w.Write([]byte(b.EnqueueMerge()))
+	userID := r.FormValue("user_id")
+
+	w.WriteHeader(200)
+	_, err = w.Write([]byte(b.EnqueueMerge(userID)))
 
 	if err != nil {
 		b.logger.Printf("Failed to write response body: %v\n", err)
 	}
 }
 
-func (b *banService) EnqueueMerge() string {
-	b.mergeQueue = append(b.mergeQueue, "foobar")
+func (b *banService) EnqueueMerge(userID string) string {
 	queueLength := len(b.mergeQueue)
 
-	if queueLength == 1 {
+	for position, enqueuedID := range b.mergeQueue {
+		if enqueuedID == userID {
+			return fmt.Sprintf("You are already in line! Your position: [%v/%v]\n", position+1, queueLength)
+		}
+	}
+
+	b.mergeQueue = append(b.mergeQueue, userID)
+
+	if queueLength == 0 {
 		return "You have the merge banhammer!"
 	}
 
-	return fmt.Sprintf("The banhammer has already been taken. Your position: [%v/%v]\n", queueLength, queueLength)
+	return fmt.Sprintf("The banhammer has already been taken. Your position: [%v/%v]\n", queueLength+1, queueLength+1)
 }
--- test/ban_service_test.go
+++ test/ban_service_test.go
@@ -16,6 +16,7 @@ func TestBanEndpoint(t *testing.T) {
 		banService := mergeban.CreateBanService()
 		requestBody := strings.NewReader("user_id=42")
 		request := httptest.NewRequest("POST", "/ban", requestBody)
+		request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 		w := httptest.NewRecorder()
 
 		banService.Ban(w, request)
@@ -30,9 +31,11 @@ func TestBanEndpoint(t *testing.T) {
 		banService := mergeban.CreateBanService()
 		requestBody := strings.NewReader("user_id=23")
 		request := httptest.NewRequest("POST", "/ban", requestBody)
+		request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 		w := httptest.NewRecorder()
 		requestBody2 := strings.NewReader("user_id=42")
 		request2 := httptest.NewRequest("POST", "/ban", requestBody2)
+		request2.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 		w2 := httptest.NewRecorder()
 
 		banService.Ban(w, request)
@@ -43,5 +46,28 @@ func TestBanEndpoint(t *testing.T) {
 
 		assert.Equal(t, 200, response2.StatusCode)
 		assert.Equal(t, "The banhammer has already been taken. Your position: [2/2]\n", string(responseBody2))
+	})
+
+	t.Run("/ban - preventing the same user from enqueuing twice", func(t *testing.T) {
+		banService := mergeban.CreateBanService()
+		requestBody := strings.NewReader("user_id=23")
+		request := httptest.NewRequest("POST", "/ban", requestBody)
+		request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+		w := httptest.NewRecorder()
+		requestBody2 := strings.NewReader("user_id=42")
+		request2 := httptest.NewRequest("POST", "/ban", requestBody2)
+		request2.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+		w2 := httptest.NewRecorder()
+		w3 := httptest.NewRecorder()
+
+		banService.Ban(w, request)
+		banService.Ban(w2, request2)
+		banService.Ban(w3, request)
+		response := w3.Result()
+
+		responseBody, _ := ioutil.ReadAll(response.Body)
+
+		assert.Equal(t, 200, response.StatusCode)
+		assert.Equal(t, "You are already in line! Your position: [1/2]\n", string(responseBody))
 	})
 }
